707f55313b0cf0d7a9cc4e043e3ba0c125c34409
mahamedbelkeir <mohamedbel@protonmail.com>
2020-07-30 18:09:21 +0200

    WIP events bus and tcp network implementation

--- events/events.go
+++ events/events.go
@@ -1,2 +1,22 @@
 package events
 
+type EventsBus struct {
+	subscribers map[string]map[string]func (Event) Response
+}
+
+
+func (e *EventsBus) subscribe(eventName, subName string, callback func (Event) Response) {
+	e.subscribers[eventName][subName] = callback
+}
+
+func (e *EventsBus) publish(subType string, event Event) map[string]Response {
+	responses := map[string]Response{}
+	for subName, func := range e.subscribers[subType] {
+		responses[subName] = func(event)
+	}
+	return responses
+}
+
+type Event struct {}
+
+type Response struct {}
--- network/tcp/server.go
+++ network/tcp/server.go
@@ -1,10 +1,28 @@
 package tcp
 
+import (
+	"net"
+)
+
 type Server struct{
 
 }
 
 
 func (s *Server) Listen() {
-	
+
+}
+
+
+func (s *Server) handleConn(conn net.Conn) {
+	// call log data event
+	// authenticate
+	// await packets
+	// on packet arrival
+		// set timeout
+		// parse packet
+		// call packet event
+		// serialize response
+		// return response
+		// repeat
 }
