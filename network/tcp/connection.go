73284099278208683704938944a9540736863c26
mahamedbelkeir <mohamedbel@protonmail.com>
2020-08-01 17:39:32 +0200

    added a method to stop connection routines

--- network/tcp/connection.go
+++ network/tcp/connection.go
@@ -10,12 +10,19 @@ type Connection struct {
 	conn net.Conn
 	SendQueue chan network.Packet
 	ReceiveQueue chan network.Packet
+	errors chan error
+	stopTransmit chan bool
+	stopRecieve chan bool
 }
 
 func EstablishConnection(conn net.Conn) Connection {
 	connection := Connection{
 		conn: conn,
 		SendQueue: make(chan network.Packet),
+		ReceiveQueue: make(chan network.Packet),
+		errors: make(chan error),
+		stopTransmit: make(chan bool),
+		stopRecieve: make(chan bool),
 	}
 	go connection.transmitData()
 	go connection.receieveData()
@@ -27,6 +34,8 @@ func (c Connection) transmitData() {
 		select {
 		case packet := <- c.SendQueue:
 			writeTCPPacket(c.conn, packet)
+		case <- c.stopTransmit:
+			return
 		}
 	}
 }
@@ -35,7 +44,16 @@ func (c Connection) receieveData() {
 	for {
 		packet := readTCPPacket(c.conn)
 		c.ReceiveQueue <- packet
+		select {
+		case <- c.stopRecieve:
+			return
+		}
 	}
+}
+
+func (c Connection) Stop() {
+	c.stopRecieve <- true
+	c.stopTransmit <- true
 }
 
 func (c Connection) Send(data network.Packet) {
