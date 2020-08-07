fb753b1d9cacb836cea306ffeff98b7d9158d69e
mahamedbelkeir <mohamedbel@protonmail.com>
2020-08-07 18:40:46 +0200

    added interface to bytes and type chekcing method, todo: add unserializing function

--- network/tcp/utils/parse.go
+++ network/tcp/utils/parse.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/json"
+	"math"
 	"io"
 	"bytes"
 	"encoding/binary"
@@ -111,15 +113,21 @@ func ParseAuth(conn net.Conn, username, password string) bool {
 
 
 var OperationsMap = map[uint8]string {
-	0x1: "GET",
-	0x2: "SET",
-	0x3: "DEL",
+	0x1: "OK",
+	0x2: "PING",
+	0x3: "GET",
+	0x4: "SET",
+	0x5: "DEL",
+	0x6: "ERR",
 }
 
 var ReverseOperationsMap = map[string]uint8 {
-	"GET": 0x1,
-	"SET": 0x2,
-	"DEL": 0x3,
+	"OK": 	0x1,
+	"PING": 0x2,
+	"GET": 	0x3,
+	"SET": 	0x4,
+	"DEL": 	0x5,
+	"ERR":  0x6,
 }
 
 var TypesMap = map[uint8]string {
@@ -127,13 +135,42 @@ var TypesMap = map[uint8]string {
 	0x2: "boolean",
 	0x3: "integer",
 	0x4: "float",
-	0x5: "struct",
+	0x5: "json",
 }
 
 var ReverseTypesMap = map[string]uint8 {
-	"string": 0x1,
-	"boolean": 0x2,
-	"integer": 0x3,
-	"float": 0x4,
-	"struct": 0x5,
+	"string": 	0x1,
+	"boolean": 	0x2,
+	"integer": 	0x3,
+	"float": 	0x4,
+	"json": 	0x5,
+}
+
+
+func Serialize(data interface{}) ([]byte, string, error) {
+	switch data.(type) {
+	case string:
+		return []byte(data.(string)), "string", nil
+	case int:
+		result := make([]byte, 8)
+		binary.LittleEndian.PutUint64(result, uint64(data.(int)))
+		return result, "integer", nil
+	case bool:
+		if data.(bool) {
+			return []byte{0x1}, "boolean", nil
+		} else {
+			return []byte{0x0}, "boolean", nil
+		}
+	case float64:
+		uintrep := math.Float64bits(data.(float64))
+		result := make([]byte, 8)
+		binary.LittleEndian.PutUint64(result, uint64(uintrep))
+		return result, "float", nil
+	default:
+	// case []interface{}:
+		result, err:= json.Marshal(data); if err != nil {
+			return nil, "err", err
+		}
+		return result, "json", nil
+	}
 }
